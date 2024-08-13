252e0375b46c3e309af9c923db6ac45ee0455952
nihal-ahmed-ts <nihal.ahmed@thoughtspot.com>
2024-08-13 20:14:39 +0530

    Update main.js
    testing with new event to force the render of user number formatter

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const numberFormatter = (value, format = '') => {
   } else if (value > 1000000 || value < -1000000) {
     formattedValue = userNumberFormatter((value / 1000000), format) + 'M';
   } else if (value > 1000 || value < -1000) {
-    formattedValue = userNumberFormatter((value / 1000), format) + 'K';
+    formattedValue = userNumberFormatter(value / 1000, format) + 'K';
   } else {
     formattedValue = userNumberFormatter(value, format);
   }
@@ -181,6 +181,8 @@ const renderChart = async (ctx) => {
       if (propKey === 'numberFormat') {
         userNumberFormat = propValue || '0,0'; // Use the default format if none provided
         renderChart(ctx); // Re-render the chart with the new format
+      } else if (propKey === 'settingsApply') {
+        renderChart(ctx); // Force re-render on settings tab apply
       }
     },
   });
